9862b00a8f47cf6ff6b5f835fbe01a657d18ff64
Andres Galaviz <andres.galaviz@gmail.com>
2016-01-23 21:53:37 +0800

    Continuos control

--- flightControl.js
+++ flightControl.js
@@ -12,102 +12,86 @@ Cylon.robot({
 	},
 
 	work: function(my){
+		var onFlight = false;
+		var stop = false;
+		var active = true;
 		my.leapmotion.on('frame', function(frame){
 			var hand = frame.hands[0];
-			if(frame.hands.length > 0 && hand.timeVisible > 1){
-				my.drone.takeoff();
-				
-				var position = hand.palmPosition;
-				var velocity = hand.palmVelocity;
-				var direction = hand.direction;
-				var normal = hand.palmNormal;
-				// TODO: Make continuos
-				if(position[1] > 280) {
-					console.log("UP");
-					my.drone.up(0.1);
-				} else if(position[1] < 120) {
-					console.log("DOWN");
-					my.drone.down(0.1);
-				} else {
-				
-					if(normal[2] > 0.1) {
-						
-						console.log("Front")
-						my.drone.forward(0.1);
-					} else if(normal[2] < -0.3) {
-						
-						console.log("Back");
-						my.drone.back(0.1);
-					} 
-					if(normal[0] > 0.3) {
-						console.log("Left")
-						my.drone.left(0.1);
+			if(frame.hands.length > 0) {
+				if(hand.timeVisible > 4) {
+					var position = hand.palmPosition;
+					var velocity = hand.palmVelocity;
+					var direction = hand.direction;
+					var normal = hand.palmNormal;
+					
+					stop = true;
+					// TODO: Make continuos
+					if(position[1] > 280) {
+						console.log("UP " + ((position[1] - 280)/100));
+						stop = false;
+						active = true;
+						my.drone.up((position[1] - 280)/100);
+					} else if(position[1] < 120) {
+						console.log("DOWN " + ((position[1] - 120)/100));
+						stop = false;
+						active = true;
+						my.drone.down((position[1] - 120)/100);
+					} else {
+						if(normal[2] > 0.3) {
+							console.log("FRONT " + (normal[2] - 0.15));
+							stop = false;
+							active = true;
+							my.drone.forward(normal[2] - 0.15);
+						} else if(normal[2] < -0.3) {
+							console.log("BACK " + (Math.abs(normal[2]) - 0.15));
+							stop = false;
+							active = true;
+							my.drone.back(Math.abs(normal[2]) - 0.15);
+						} 
+						if(normal[0] > 0.3) {
+							console.log("LEFT " + (normal[0] - 0.2));
+							stop = false;
+							active = true;
+							my.drone.left(normal[0] - 0.2);
 
-					} else if(normal[0] < -0.3) {
-						console.log("Right")
-						my.drone.right(0.1);
+						} else if(normal[0] < -0.3) {
+							console.log("RIGHT " + (Math.abs(normal[0]) - 0.3));
+							stop = false;
+							active = true;
+							my.drone.right(Math.abs(normal[0]) - 0.3);
+						} 
+
+					} 	
+					if(stop && active) {
+						console.log("Stop");
+						my.drone.up(0);
+						my.drone.down(0);
+						my.drone.forward(0);
+						my.drone.back(0);
+						my.drone.left(0);
+						my.drone.right(0);
+						stop = false;
+						active = false;
+					}	
+				} else if(hand.timeVisible > 0.5) {
+					if(!onFlight){
+						console.log("Take off");
+						my.drone.takeoff();	
+						onFlight = true;
 					}
+					
 				}
 			} else if(frame.hands.length == 0){
-				my.drone.land();
+				
+				
+				if(onFlight){
+					console.log("Landing");	
+					my.drone.land();
+					onFlight = false;
+				}
 			}
 
 			
-			
-
-			// if(frame.valid && frame.gestures.length > 0){
-			// 	frame.gestures.forEach(function(g){
-			// 		if(g.type == 'swipe'){
-			// 			var currentPosition = g.position;
-			// 			var startPosition = g.startPosition;
-
-			// 			var xDirection = currentPosition[0] - startPosition[0];
-			// 			var yDirection = currentPosition[1] - startPosition[1];
-			// 			var zDirection = currentPosition[2] - startPosition[2];
-
-			// 			var xAxis = Math.abs(xDirection);
-			// 			var yAxis = Math.abs(yDirection);
-			// 			var zAxis = Math.abs(zDirection);
-
-			// 			var superiorPosition  = Math.max(xAxis, yAxis, zAxis);
-
-			// 			if(superiorPosition === xAxis){
-			// 				if(xDirection < 0){
-			// 					console.log('LEFT');
-			// 					my.drone.left(1);
-			// 				} else {
-			// 					my.drone.right(1);
-			// 					console.log('RIGHT');
-			// 				}
-			// 			}
-
-			// 			if(superiorPosition === zAxis){
-			// 				if(zDirection > 0){
-			// 					console.log('BACKWARDS');
-			// 					my.drone.back(1);
-			// 				} else {
-			// 					console.log('FORWARD');
-			// 					my.drone.forward(1);
-			// 				}
-			// 			}
-
-			// 			if(superiorPosition === yAxis){
-			// 				if(yDirection > 0){
-			// 					console.log('UP');
-			// 					my.drone.up(1);
-			// 				} else {
-			// 					console.log('DOWN');
-			// 					my.drone.down(1);
-			// 				}
-			// 			}
-			// 		} else if(g.type === 'keyTap'){
-			// 			my.drone.backFlip();
-			// 			after((5).seconds(), function(){
-			// 				my.drone.land();
-			// 			})
-			// 		}
-			// 	})
-			// }
 		})
 	}
 }).start();
